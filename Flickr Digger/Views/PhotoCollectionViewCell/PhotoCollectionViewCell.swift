3229b79d668e398b3f667d38313a4821a61111ab
Ikjot SIngh <ikjotsingh1418@gmail.com>
2019-09-03 00:25:04 +0530

    .

--- Flickr Digger/Views/PhotoCollectionViewCell/PhotoCollectionViewCell.swift
+++ Flickr Digger/Views/PhotoCollectionViewCell/PhotoCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var imageView: URLImageView!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
